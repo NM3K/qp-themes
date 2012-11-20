125853e7be239ebb2983ad44fbaefe8accd46c37
fedeisas <fedeisas@hotmail.com>
2012-11-20 16:01:18 -0300

    images

--- qp-seo/inc/2-cols.php
+++ qp-seo/inc/2-cols.php
@@ -55,7 +55,7 @@
 			<div>
 				<img src="inc500.png" alt="" class="left">
 				<p><strong>We're one of America's fastest growing provate companies!</strong></p>
-				<p>Survey Analytics (our parent company) place 172nd with 1390% growth.</p>
+				<p style="padding-left:155px;">Survey Analytics (our parent company) place 172nd with 1390% growth.</p>
 			</div>
 
 			<br>
