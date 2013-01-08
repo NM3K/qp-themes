03e5d61e31d40df08404c35d4d597a1122aaa5d2
fedeisas <fedeisas@hotmail.com>
2013-01-08 19:29:24 -0300

    prevent cache

--- qp-theme-switcher/partials/assets.php
+++ qp-theme-switcher/partials/assets.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/style.css?t=<?=time()?>">
 <link rel="stylesheet" href="css/feature-carousel.css">
 <script type="text/javascript" src="js/jquery.featureCarousel.js"></script>
 <script type="text/javascript" src="js/theme-picker.js"></script>
